aefbe94d3ede8808b7ecd591df77aae35a19a742
Unknown <zac.hokanson@gmail.com>
2017-08-28 19:22:42 -0600

    Added Animal Class Scripts

---
+++ 1600Fall2017/Assets/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Dies();
+	}
+	
+	void Dies(){
+		print(name + " Dies");
+	}
+
+}
