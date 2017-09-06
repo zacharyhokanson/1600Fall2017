840e27c81b8708cd0829fbcd8aebf490d3205c46
Unknown <zac.hokanson@gmail.com>
2017-09-06 16:51:39 -0600

    Added SyntaxHomework

---
+++ 1600Fall2017/Assets/SyntaxHomework.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SyntaxHomework : MonoBehaviour {
+
+	// Use this for initialization
+	/*Syntax it an crucial part to programming, and understanding it makes debugging code a lot easier.  */
+
+	int myFirstInt = 6; //semicolons are used to end every, single line, of code. 
+	char myFirstChar = 'A'; 
+	string myFirstString = "Hello World"; //double quotes are used to mark the beginning and end of strings. 
+	char nullChar = '\n'; 
+	int mySecondInt = 25;
+
+
+
+
+
+	void Start () {//parenths are used when calling or defining a fucntion, within them is where you will pass in arguments. 
+		print(myFirstInt) /*if there is an error in your code the easiest way to figure it it out is hovering over 
+		the red squiggled underline. In this case there is a missing semicolon. */
+
+	}
+	
+	// Update is called once per frame
+	void Update () {//curly braces mark the beginnging and end of classes, or code blocks. 
+		
+
+	}
+}
