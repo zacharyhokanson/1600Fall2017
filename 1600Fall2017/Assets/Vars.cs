483e9db2e11eab653cc399d351fdef1e863d1e5a
Zac Hokanson <zac.hokanson@gmail.com>
2017-09-08 11:33:34 -0600

    Added Powerup Scene

--- 1600Fall2017/Assets/Vars.cs
+++ 1600Fall2017/Assets/Vars.cs
@@ -6,17 +6,22 @@ public class Vars : MonoBehaviour {
 	public float health = 100;
 	// Use this for initialization
 	void Start () {
+		health = 200;
 		
 	}
+
+		void OnTriggerEnter()
+		{
+			health += 20;
+		}
 	
 	// Update is called once per frame
 	void Update () {
-		if (health > 0){
-			health -= 0.1f;
-		} 
-		else{
-			print ("You Died");
-		}
+		// if (health > 0){
+		// 	health -= 0.1f;
+		// } else{
+		// 	print ("You Died");
+		// }
 		
 	}
 }
