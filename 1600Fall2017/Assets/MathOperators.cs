3b11be3a1f2dcc908e2644a3b2a491b18e13b2f8
Unknown <zac.hokanson@gmail.com>
2017-09-08 19:05:45 -0600

    Added Math Operators Homework

---
+++ 1600Fall2017/Assets/MathOperators.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathOperators : MonoBehaviour {
+/*Math operators are used to do math operations. Things like + - / * =. 
+Anything that does math works as a math operator. */
+
+int x = 4;
+int y = 6;
+int z = 3;
+	// Use this for initialization
+	void Start () {
+		print(x+y);// Adds x and y
+		print(x*y);// Multiplies x and y
+		y -= 20;// these operators change and reassign the variable they are placed after. This one subracts 20 to y
+		y += 10;// this one adds 10 to y and reassigns y to the new value. 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
