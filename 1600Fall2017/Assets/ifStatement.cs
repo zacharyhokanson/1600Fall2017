aa70d50c3be229cce0adff9ed841476db6a8fb39
Zac Hokanson <zac.hokanson@gmail.com>
2017-09-11 11:49:56 -0600

    Updated ifStatment

--- 1600Fall2017/Assets/ifStatement.cs
+++ 1600Fall2017/Assets/ifStatement.cs
@@ -1,20 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class ifStatement : MonoBehaviour {
-	public int num1;
-	public int num2;
-	public int value;
+	// public int num1;
+	// public int num2;
+	// public int value;
+	// public bool canPlay = true;
+	public Text input;
+	public string password = "abcd";
 
 	// Use this for initialization
-	void Start () {
-		if (num1 + num2 == value)
-		{
-			print(value);
+	void Update () {
+		// if (num1 + num2 == value)
+		// {
+		// 	print(value);
+		// }
+		// if (canPlay)
+		// {
+		// 	print("Play");
+		// }
+		if (input.text == password){
+			print(":D");
 		}
-		
 	}
-	
-
 }
