b10ec36f43138c65d5b87c5e43135efeb03b9e62
Anthony Romrell <anthonyromrell@mac.com>
2017-09-13 11:45:21 -0600

    if Statements

---
+++ 1600Fall2017/Assets/MyClass.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyClass : MonoBehaviour {
+
+	string myString = "Hello";
+
+	void Start () {
+		print(myString);
+	}
+	
+}
---
+++ 1600Fall2017/Assets/ifStatement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ifStatement : MonoBehaviour {
+
+	public Text input;
+
+	public bool canPlayGame = false;
+	public string password = "G@am3Pl@y";
+
+	void Update () {
+		if(input.text == password) {
+			print("You know the password.");
+			canPlayGame = true;
+		} else {
+			print("The password is incorrect.");
+		}
+
+		if(canPlayGame) {
+			print("Playing Game");
+		}	else {
+			print("Can't Play Yet, Enter a Correct Password.");
+		}
+	}
+}
