88dd4b619f1fa8ad901857ed401db84e0d0ab8c2
Unknown <zac.hokanson@gmail.com>
2017-09-18 09:17:12 -0600

    Added Switch Statments

---
+++ 1600Fall2017/Assets/SwitchStatments.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchStatments : MonoBehaviour {
+	public int cardNumber;
+	public char letterGrade;
+	// Use this for initialization
+	void Start () {
+		switch (cardNumber)
+		{	
+			case 1:
+				print("Ace");
+				break;
+			case 11:
+				print("Jack");
+				break;
+			case 12:
+				print("Queen");
+				break;
+			case 13:
+				print("King");
+				break;
+			
+			default:
+				print(cardNumber);
+				break;
+		}
+		switch (letterGrade)
+		{
+			case 'A':
+			case 'B':
+			case 'C':
+				print("Pass");
+				break;
+			case 'F':
+				print("Fail");
+				break;
+
+			default:
+				print("Invalid Letter Grade");
+				break;
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
