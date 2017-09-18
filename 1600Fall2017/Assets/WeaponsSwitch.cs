b7d558b14ad15d75300d9cd9bf13088e1ab0d889
Zac Hokanson <zac.hokanson@gmail.com>
2017-09-18 11:49:44 -0600

    Updated Charater Scripts

--- 1600Fall2017/Assets/WeaponsSwitch.cs
+++ 1600Fall2017/Assets/WeaponsSwitch.cs
@@ -6,4 +6,21 @@ public class WeaponsSwitch : MonoBehaviour {
 
 	public string[] weapons;
 
+	
+
+	public void MyWeaponSwitch(int i) {
+		switch (weapons[i])
+		{
+			case "Missile" :
+				print("Missile");
+				break;
+			case "Plasma" :
+				print("Plasma");
+				break;
+			default:
+				print("You have no other weapons.");
+				break;
+		}
+	}
+
 }
