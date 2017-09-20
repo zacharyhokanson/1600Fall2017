790e1e0cb348294e4d894d3d3d369bcf6d00cdc0
Zac Hokanson <zac.hokanson@gmail.com>
2017-09-20 11:42:19 -0600

    Updated Scripts While Loops

---
+++ 1600Fall2017/Assets/Loops.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour {
+
+public bool canPlay = true;
+
+void Start() {
+
+	StartCoroutine(PlayGame());
+
+}
+
+IEnumerator PlayGame() {
+	while(canPlay){
+		print("Play.");
+		yield return new WaitForSeconds(0.1f);
+	}
+	print("Game Over.");
+}
+
+
+}
