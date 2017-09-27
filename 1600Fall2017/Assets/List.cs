0ce2146225137d670d1d95b276622f3508751ba5
Zac Hokanson <zac.hokanson@gmail.com>
2017-09-27 11:26:43 -0600

    Added List Script

---
+++ 1600Fall2017/Assets/List.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class List : MonoBehaviour {
+
+	public List<GameObject> myWeapons;
+
+	void OnTriggerEnter(Collider other)
+	{
+		
+	}
+}
