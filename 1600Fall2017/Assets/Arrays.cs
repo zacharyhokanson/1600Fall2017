16a7158503d9d7a380090b46d872c14b0e2382b1
Unknown <zac.hokanson@gmail.com>
2017-09-27 09:10:47 -0600

    Updated Homwork assignments.

---
+++ 1600Fall2017/Assets/Arrays.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour {
+	/*Arrays are used to hold various objects of the same type */
+		char[] Grades = new char[4] {'A', 'B', 'C', 'F'};
+		int[] Scores = new int[3] {97, 81, 32};
+		string[] Response = new string[2]{"Good Job!", "Try Again."};
+		string[] Names = new string[3]{"David", "Susan", "Billy"};
+		int[] IntTwo = new int[5]{1, 5, 3, 2, 8};
+		int[] Stats = new int[3]{12, 16, 48};
+		string[] WeaponNames = new string[3]{"Gun", "Missile", "Plasma"};
+		string[] AreaNames = new string[3]{"Zone 1", "Zone 2 ", "Zone 3"};
+		string[] EnemyNames = new string[2]{"Skeleton", "Zombie"};
+		string[] MusicTrakcs = new string[3]{"Track 1", "Track 2", "Track 3"};
+
+	// Use this for initialization
+	void Start () {
+
+		GradeScores(Scores);
+		PrintStrings(Names);
+		PrintInt(IntTwo);
+	}
+	
+	public void GradeScores(int[] x){
+		for (int i = 0; i < x.Length; i++)
+		{
+			if (x[i] >= 90)
+			{
+				print(Grades[0] + " " + Response[0]);
+			} 
+			else if (x[i] < 90 && x[i] >= 80)
+			{
+				print(Grades[1] + " " + Response[0]);
+			} 
+			else if (x[i] < 80 && x[i] >= 70)
+			{
+				print(Grades[2] + " " +Response[0]);
+			} 
+			else
+			{
+				print(Grades[3] + " " + Response[1]);
+			}
+
+		}
+	}
+	public void PrintStrings(string[] Strings){
+		foreach (string i in Strings) //using a for loop you can cyle through the various item in an array.
+		{
+			print(i);
+		}
+	}
+
+	public void PrintInt(int[] x){
+		foreach (int i in x)
+		{
+			print(i);
+		}
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 1600Fall2017/Assets/WhileLoops.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhileLoops : MonoBehaviour {
+	public int health = 100;
+	public int mana = 100;
+	public int stamina = 100;
+	public bool CanPlay = true;
+	public bool Armed = true;
+
+	// Use this for initialization
+	void Start () {
+		while (true)// while loops, like if staments check for truth. 
+		{
+			print("True");
+		}
+		while (false)// if a statment will always be false the code block within it will never been see/used. 
+		{
+			print("This will never be seen.");
+		}
+		while (health > 0)// checks if the first value is greater than the second.
+		{
+			health = health - 1;
+			//print(health);
+			print("Alive");
+		}
+		while (mana > 100)
+		{
+			print("Can cast spells");
+		}
+		while (stamina > 100 ){
+			if (stamina > 50)
+			{
+				print("Run");
+				stamina = stamina - 1;
+			}
+			if (stamina < 50){ //checks if the first value is les than the second. 
+				print("Walk");
+				stamina = stamina +1;
+			}
+		}
+		while (CanPlay)
+		{
+			print("Can Play");
+		}
+		while (Armed)
+		{
+			print("Shoot");
+		}
+		while (health == 100)//compares the two values and checks if the are equal.
+		{
+			print("Full Health");	
+		}
+		
+		while (health == 100 && mana == 100 && stamina == 100)// && stands for and. it checks if multiple things are true
+		{
+			print("Full Stats");
+		}
+		while (health < 50 || mana < 50 || stamina < 50)//|| stands for or. it checks if any one of the things in the statment is true. 
+		{
+			print("Need to recover.");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
