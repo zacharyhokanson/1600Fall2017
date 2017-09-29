0d0cf2a82ae18528c816e9e690f7d6af0132991c
Zac Hokanson <zac.hokanson@gmail.com>
2017-09-29 11:23:04 -0600

    List Scrips Homework

---
+++ 1600Fall2017/Assets/Homeworks/Lists.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lists : MonoBehaviour {
+		List<string> monsters = new List<string>();
+
+	// Use this for initialization
+	void Start () {
+		monsters.Add("Skeleton");
+		monsters.Add("Zombie");
+		monsters.Add("Wraith");
+
+		foreach (string i in monsters)
+		{
+			print(i);
+		}
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
