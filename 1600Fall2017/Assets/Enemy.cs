eee3bd61c4cdec1765d76b21db76e052badb2df7
Zac Hokanson <zac.hokanson@gmail.com>
2017-10-11 11:54:04 -0600

    NavMesh

---
+++ 1600Fall2017/Assets/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour {
+
+	public NavMeshAgent agent;
+	public Transform player;
+	// Update is called once per frame
+	void Update () {
+		agent.destination = player.position;
+	}
+	public void Stun(float _speed){
+		agent.speed = _speed;
+		// agent.acceleration = _speed;
+		// agent.destination = transform.position;
+	}
+}
