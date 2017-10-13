2ebbddcae79215d5dbfbaa848857a39bbff02867
Zac Hokanson <zac.hokanson@gmail.com>
2017-10-13 11:53:23 -0600

    Updated

--- 1600Fall2017/Assets/SaveCheckpoint.cs
+++ 1600Fall2017/Assets/SaveCheckpoint.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SaveCheckpoint : MonoBehaviour {
 
+	public Transform player;
 	Vector3 checkPoint;
 	string[] playerPrefsTitles = {"CheckPointX", "CheckPointY", "CheckPointZ"};
 
@@ -14,10 +15,17 @@ public class SaveCheckpoint : MonoBehaviour {
 		for (int i = 0; i < playerPrefsTitles.Length; i++)
 		{
 			PlayerPrefs.SetFloat(playerPrefsTitles[i], checkPoint[i]);
-			print(PlayerPrefs.GetFloat(playerPrefsTitles[i]));
 		}		
 	}
 
-
+	void Start()
+	{
+		for (int i = 0; i < playerPrefsTitles.Length; i++)
+		{
+			checkPoint[i] = PlayerPrefs.GetFloat(playerPrefsTitles[i]);
+		}
+		
+		player.position = checkPoint;
+	}
 
 }
