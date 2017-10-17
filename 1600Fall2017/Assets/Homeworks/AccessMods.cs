8cb369ce2be466ac99082af44cf54fa23049ddec
Unknown <zac.hokanson@gmail.com>
2017-10-16 18:42:26 -0600

    Auto stash before merge of "master" and "origin/master"

---
+++ 1600Fall2017/Assets/Homeworks/AccessMods.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccessMods : MonoBehaviour {
+		
+		int numberOne; //unless explicitly stated a variable is defined as private
+		public int numberTwo;// public means anything can see it. 
+		private int numberThree;//private means that only the script in which it is defined in can see it. 
+	// Use this for initialization
+	void Start () {
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
