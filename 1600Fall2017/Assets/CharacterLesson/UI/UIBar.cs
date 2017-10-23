1da92824ed06e550fce8f80edc5a43a9fbe0118b
Zac Hokanson <zac.hokanson@gmail.com>
2017-10-23 11:55:37 -0600

    Update

---
+++ 1600Fall2017/Assets/CharacterLesson/UI/UIBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBar : MonoBehaviour {
+
+	public Image bar;
+	public float barTime = 0.1f;
+
+	void OnTriggerEnter () {
+		StartCoroutine(UpdateBar());	
+	}
+
+	IEnumerator UpdateBar() {
+		while (bar.fillAmount < 1){
+			bar.fillAmount += barTime;
+			yield return new WaitForSeconds(barTime);
+		}
+	}
+
+}
