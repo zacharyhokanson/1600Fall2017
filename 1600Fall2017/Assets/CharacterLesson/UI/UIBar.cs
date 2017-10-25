c7fc9d2bb1d5b6d63a4b95a691315336fca6128a
Zac Hokanson <zac.hokanson@gmail.com>
2017-10-25 11:54:23 -0600

    Updated PowerUps

--- 1600Fall2017/Assets/CharacterLesson/UI/UIBar.cs
+++ 1600Fall2017/Assets/CharacterLesson/UI/UIBar.cs
@@ -7,14 +7,38 @@ public class UIBar : MonoBehaviour {
 
 	public Image bar;
 	public float barTime = 0.1f;
+	public float powerLevel = 0.1f;
+	public float amountToAdd = 0.01f;
+	public enum PowerUpType {
+		PowerUp, 
+		PowerDown
+	}
+	public PowerUpType powerUp;
 
 	void OnTriggerEnter () {
-		StartCoroutine(UpdateBar());	
+		switch (powerUp)
+		{
+			case PowerUpType.PowerUp:
+				StartCoroutine(PowerUpBar());
+				break;
+			case PowerUpType.PowerDown:
+				StartCoroutine(PowerDownBar());
+				break;
+		}
 	}
 
-	IEnumerator UpdateBar() {
-		while (bar.fillAmount < 1){
-			bar.fillAmount += barTime;
+	IEnumerator PowerUpBar() {
+		while (bar.fillAmount < 1) {
+			bar.fillAmount += amountToAdd;
+			yield return new WaitForSeconds(barTime);
+		}
+	}
+	IEnumerator PowerDownBar() {
+		float tempAmount = powerLevel;
+		float fillAmount = bar.fillAmount;
+		while (tempAmount > 0) {
+			fillAmount = tempAmount - amountToAdd;
+			bar.fillAmount = fillAmount;
 			yield return new WaitForSeconds(barTime);
 		}
 	}
