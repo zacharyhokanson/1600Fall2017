15cc62f26d01d87260dfa256f26107abb8806518
Zac Hokanson <zac.hokanson@gmail.com>
2017-10-30 11:05:10 -0600

    Added To Scene

--- 1600Fall2017/Assets/CharacterLesson/CharacterControl.cs
+++ 1600Fall2017/Assets/CharacterLesson/CharacterControl.cs
@@ -7,7 +7,7 @@ public class CharacterControl : MonoBehaviour {
 	public static bool gameOver;
 	public float gravity = 9.81f;
 	public float jumpForce = 150;
-	public float speed = 50;
+	public float speed = 30;
 	public Vector3 moveVector3;
 	public CharacterController characterController;
 	void Start () {
