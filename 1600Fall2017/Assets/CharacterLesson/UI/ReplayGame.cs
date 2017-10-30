18c69812cc3f26d4b44d022fb67a1cc40e1dad59
Zac Hokanson <zac.hokanson@gmail.com>
2017-10-30 11:53:42 -0600

    Added Replay and Checkpoints

---
+++ 1600Fall2017/Assets/CharacterLesson/UI/ReplayGame.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReplayGame : MonoBehaviour {
+
+	public Transform player;
+	public Image uiBar;
+	public GameObject GameOverUI;
+	public static Vector3 startPosition;
+	private float fillAmount;
+
+	public void Awake() {
+		GameOverUI.SetActive(false);
+		startPosition = player.position;
+		fillAmount = uiBar.fillAmount;
+	}
+	public void Click() {
+		player.position = startPosition;
+		uiBar.fillAmount = fillAmount;
+		GameOverUI.SetActive(false);
+		CharacterControl.gameOver = false;
+	}
+
+
+
+}
