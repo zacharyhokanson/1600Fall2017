25608f656773ccf169b4849a5421ef5f31e815d6
Zac Hokanson <zac.hokanson@gmail.com>
2017-11-02 16:22:48 -0600

    Added Camera Follow

---
+++ 1600Fall2017/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform player;
+	public Vector3 offset;
+
+	void LateUpdate() {
+
+		transform.position = player.position + offset;
+	}
+}
