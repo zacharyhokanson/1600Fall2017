7cea6a7c3c9ea45c22db9f2993c6eb5ab3b73d60
Zac Hokanson <zac.hokanson@gmail.com>
2017-11-02 23:20:30 -0600

    HealthExperiment

---
+++ 1600Fall2017/Assets/Scripts/UI/ChangeHealth.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeHealth : MonoBehaviour {
+	public Image bar;
+	public GameObject gameOverUI;
+	public float powerLevel = 0.1f;
+	public float amountToAdd = 0.01f;
+	public void HealthUp() {
+		StartCoroutine(PowerUpBar());
+	}
+	public void HealthDown(){
+		StartCoroutine(PowerDownBar());
+	}
+	public IEnumerator PowerUpBar() {
+			float tempAmount = bar.fillAmount + powerLevel;
+			if (tempAmount > 1) {
+				tempAmount = 1;
+			}
+			while (bar.fillAmount < tempAmount) {
+				bar.fillAmount += amountToAdd;
+				yield return new WaitForSeconds(amountToAdd);
+			}
+		}
+	public IEnumerator PowerDownBar() {
+		float tempAmount = bar.fillAmount - powerLevel;
+		if (tempAmount < 0) {
+			tempAmount = 0;
+		}
+		while (bar.fillAmount > tempAmount) {
+			bar.fillAmount -= amountToAdd;
+		
+			yield return new WaitForSeconds(amountToAdd);
+
+			if (bar.fillAmount == 0) {
+				gameOverUI.SetActive(true);
+				CharacterControl.gameOver = true;
+			}
+		}
+	}
+}
+
--- 1600Fall2017/Assets/Scripts/UI/PowerUps.cs
+++ 1600Fall2017/Assets/Scripts/UI/PowerUps.cs
@@ -5,10 +5,9 @@ using UnityEngine.UI;
 
 public class PowerUps : MonoBehaviour {
 
-	public Image bar;
-	public GameObject gameOverUI;
-	public float powerLevel = 0.1f;
-	public float amountToAdd = 0.01f;
+	// public Image bar;
+	// public GameObject gameOverUI;
+
 	public enum PowerUpType {
 		PowerUp, 
 		PowerDown,
@@ -20,42 +19,14 @@ public class PowerUps : MonoBehaviour {
 		switch (powerUp)
 		{
 			case PowerUpType.PowerUp:
-				StartCoroutine(PowerUpBar());
+				ChangeHealth.HealthUp();
 				break;
 			case PowerUpType.PowerDown:
-				StartCoroutine(PowerDownBar());
+				ChangeHealth.HealthDown();
 				break;
 			case PowerUpType.Jump:
 				CharacterControl.airJump = 3;
 				break;
 		}
-	}
-
-	IEnumerator PowerUpBar() {
-		float tempAmount = bar.fillAmount + powerLevel;
-		if (tempAmount > 1) {
-			tempAmount = 1;
-		}
-		while (bar.fillAmount < tempAmount) {
-			bar.fillAmount += amountToAdd;
-			yield return new WaitForSeconds(amountToAdd);
-		}
-	}
-	IEnumerator PowerDownBar() {
-		float tempAmount = bar.fillAmount - powerLevel;
-		if (tempAmount < 0) {
-			tempAmount = 0;
-		}
-		while (bar.fillAmount > tempAmount) {
-			bar.fillAmount -= amountToAdd;
-			
-			yield return new WaitForSeconds(amountToAdd);
-
-			if (bar.fillAmount == 0) {
-				gameOverUI.SetActive(true);
-				CharacterControl.gameOver = true;
-			}
-		}
-	}
-
+	}	
 }
