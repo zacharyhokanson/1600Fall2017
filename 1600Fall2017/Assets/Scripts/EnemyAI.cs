b2d65481562753e9814b7bb9a08be3d617a64f0a
Zac Hokanson <zac.hokanson@gmail.com>
2017-11-06 11:44:48 -0700

    Added Enemies

---
+++ 1600Fall2017/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour {
+
+	public Transform player;
+	public NavMeshAgent agent;
+
+	// Update is called once per frame
+	void Update () {
+		agent.destination = player.position;
+	}
+}
