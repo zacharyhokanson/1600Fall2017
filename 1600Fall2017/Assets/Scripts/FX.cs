364f2e754d497fff06c9d30a545a1409a20d3dd3
Zac Hokanson <zac.hokanson@gmail.com>
2017-11-10 11:46:32 -0700

    Added Particles

---
+++ 1600Fall2017/Assets/Scripts/FX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FX : MonoBehaviour {
+	public Transform start;
+	public Transform end;
+	public LineRenderer line;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//line.SetPositions(0, start);
+		//line.SetPositions(end, 1);
+	}
+}
