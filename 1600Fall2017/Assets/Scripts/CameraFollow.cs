b451937df680ac0b37099b315430c5b7b9e55c36
Zac Hokanson <zac.hokanson@gmail.com>
2017-11-15 11:35:49 -0700

    Added Level 2

--- 1600Fall2017/Assets/Scripts/CameraFollow.cs
+++ 1600Fall2017/Assets/Scripts/CameraFollow.cs
@@ -6,8 +6,11 @@ public class CameraFollow : MonoBehaviour {
 	public Transform player;
 	public Vector3 offset;
 
+	void Start() {
+		offset.z = -45;
+	}
 	void LateUpdate() {
-
+		
 		transform.position = player.position + offset;
 	}
 }
