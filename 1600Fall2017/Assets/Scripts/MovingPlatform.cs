c9f7764c6e2c4e80e39abd58863601659a671ec7
Zac Hokanson <zac.hokanson@gmail.com>
2017-11-27 11:25:33 -0700

    Added Moving Platform Script

---
+++ 1600Fall2017/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+	public Vector3 moveVector3;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
