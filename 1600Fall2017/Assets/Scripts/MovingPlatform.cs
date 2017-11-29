aecb05c23a5caf50da9c5708b3bef15d828278c0
Zac Hokanson <zac.hokanson@gmail.com>
2017-11-29 11:49:35 -0700

    Updated Moving Platform

--- 1600Fall2017/Assets/Scripts/MovingPlatform.cs
+++ 1600Fall2017/Assets/Scripts/MovingPlatform.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour {
+	public float speed = 10;
 	public Vector3 moveVector3;
-	// Use this for initialization
+	// use character controller to handle movement 
+	//have check for left and right bounds 
+	public CharacterController characterController;
 	void Start () {
-		
+		moveVector3.x = speed;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+		characterController.Move(moveVector3);
 	}
 }
