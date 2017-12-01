11b699c2f63a28fbcd0c386b39b96eb6eb02821c
Zac Hokanson <zac.hokanson@gmail.com>
2017-12-01 09:47:37 -0700

    Added Point Patrol

---
+++ 1600Fall2017/Assets/Scripts/PointPatrol.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointPatrol : MonoBehaviour {
+
+	public Transform[] points;
+	public int speed;
+	public Vector3 pointA;
+	public Vector3 pointB;
+
+	void Start() {
+		UpdateTarget();
+	}
+	void UpdateTarget(){
+		pointA = transform.position;
+		pointB = points[1].position;
+
+	}
+
+	void Update() {
+		float step = speed * Time.deltaTime;
+		//transform.position =  Vector3.MoveTowards(pointA, pointB, step);
+
+	}
+ }
